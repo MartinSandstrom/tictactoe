81fb08bdf466933ad949741bda24fb53ce13ded7
Martin Sandström <martin.sandstrom@postkodlotteriet.se>
2017-07-13 12:52:52 +0200

    Adding correct logic for correcting the field

--- src/tictactoe/core.cljs
+++ src/tictactoe/core.cljs
@@ -4,63 +4,90 @@
 (enable-console-print!)
 
 (defn new-board [n]
-    (vec (repeat n (vec (repeat n "B")))))
+  (vec (repeat n (vec (repeat n "B")))))
 
-    
-(def board-size 3)
-    
+(def board-size 5)
+
 (println (new-board board-size))
 
-(defonce app-state (atom 
-    {:text "Hello world from clojure!"
-     :board (new-board board-size)}))
-     
- (defn computer-move []
-     (swap! app-state assoc-in [:board 0 0] "C"))
- 
+(defonce app-state (atom
+                    {:text "Hello world from clojure!"
+                     :board (new-board board-size)}))
+
+(defn computer-move [board]
+  (prn board)
+  (let [remaining-spots (for [i (range board-size)
+                              j (range board-size)
+                              :when (= (get-in board [j i]) "B")]
+                          [j i])
+        move (when (seq remaining-spots)
+               (rand-nth remaining-spots))]
+    (if move
+      (assoc-in board move "C")
+      board)))
+
+(defn straight [owner board [x y] [dx dy] n]
+  (every? true?
+          (for [i (range n)]
+            (= (get-in board [(+ (* dx i) x)
+                              (+ (* dy i) y)])
+               owner))))
+
+(defn win? [owner board n]
+  (some true?
+        (for [i (range board-size)
+              j (range board-size)
+              dir [[1 0] [0 1] [1 1] [1 -1]]]
+          (straight owner board [i j] dir n))))
+
+(defn check-for-winner [player]
+  (if (win? player (:board @app-state) board-size) (js/alert (str player " winning")) ()))
+
 (defn blank [i j]
-[:rect {:width 0.9 
-         :height 0.9 
-         :fill "grey"
-         :x i 
-         :y j
-         :on-click (fn rect-click [e] 
-             (swap! app-state assoc-in [:board i j ] "P")
-             (computer-move))}])
- 
- (defn circle [i j]
- [:circle {:r 0.45
-           :cx (+ 0.5 i)
-           :cy (+ 0.5 j)
-           :fill "red"}])
+  [:rect {:width 0.9
+          :height 0.9
+          :fill "grey"
+          :x i
+          :y j
+          :on-click (fn rect-click [e]
+                      (swap! app-state assoc-in [:board i j] "P")
+                      (check-for-winner "P")
+                      (swap! app-state update-in [:board] computer-move)
+                      (check-for-winner "C"))}])
+
+(defn circle [i j]
+  [:circle {:r 0.45
+            :cx (+ 0.5 i)
+            :cy (+ 0.5 j)
+            :fill "red"}])
 
 (defn cross [i j]
-[:g {:stroke "black" 
-    :stroke-width 0.35
-    :stroke-linecap "round"
-    :transform (str "translate(" (+ 0.5 i) "," (+ 0.5 j) ") "
-            "scale(0.35)")}
-  [:line {:x1 -1 :y1 -1 :x2 1 :y2 1}]
-  [:line {:x1 -1 :y1 1 :x2 1 :y2 -1}]])
+  [:g {:stroke "black"
+       :stroke-width 0.35
+       :stroke-linecap "round"
+       :transform (str "translate(" (+ 0.5 i) "," (+ 0.5 j) ") "
+                       "scale(0.35)")}
+   [:line {:x1 -1 :y1 -1 :x2 1 :y2 1}]
+   [:line {:x1 -1 :y1 1 :x2 1 :y2 -1}]])
 
 (defn tictactoe []
-  [:center 
-  [:h1 (:text @app-state)]
-  [:svg {:view-box (str "0 0 " board-size " " board-size)
-         :width 500 
-         :height 500}
-    (doall 
-        (for [i (range (count (:board @app-state)))
-              j (range (count (:board @app-state)))]
-          (case (get-in @app-state [:board i j])
-              "B" [blank i j]
-              "P" [circle i j]
-              "C" [cross i j])))]
-  [:p 
-    [:button 
-        {:on-click 
-            (fn new-game-click[e] 
-            (swap! app-state assoc :board (new-board board-size)))} "New game"]]])
+  [:center
+   [:h1 (:text @app-state)]
+   [:svg {:view-box (str "0 0 " board-size " " board-size)
+          :width 500
+          :height 500}
+    (doall
+     (for [i (range (count (:board @app-state)))
+           j (range (count (:board @app-state)))]
+       (case (get-in @app-state [:board i j])
+         "B" [blank i j]
+         "P" [circle i j]
+         "C" [cross i j])))]
+   [:p
+    [:button
+     {:on-click
+      (fn new-game-click [e]
+        (swap! app-state assoc :board (new-board board-size)))} "New game"]]])
 
 (reagent/render-component [tictactoe]
                           (. js/document (getElementById "app")))
@@ -69,5 +96,4 @@
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-  (swap! app-state assoc-in [:text] "Hellow world from text")
-)
+  (swap! app-state assoc-in [:text] "Hellow world from text"))
